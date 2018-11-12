6b967c3d8bf7af6ed5b8158f07aed06bf41dd2dd
Marco Köper <info@koeper-it.de>
2018-11-12 14:36:46 +0100

    first

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function helloAction()
+    {
+        return $this->Render("default/index.html.twig");
+    }
+}
